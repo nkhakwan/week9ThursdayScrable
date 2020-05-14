11d02c66e51a43e08b90f04d722137c9e84fe85a
nkhakwan <post_khan@yahoo.com>
2020-05-14 13:22:43 -0700

    committing after populateing more code

---
+++ Scrable/Models/Backend.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+
+namespace WordValue{
+  public class MyWord
+  {
+
+    public static List<char> OneV = new List<char>{'a', 'e', 'i', 'o', 'u', 'l', 'n', 'r', 's', 't'};
+    public static List<char> TwoV = new List<char>();
+    public static List<char> ThreeV = new List<char>();
+    public static List<char> FourV = new List<char>();
+    public static List<char> FiveV = new List<char>();
+    public static List<char> EightV = new List<char>();
+    public static List<char> TenV = new List<char>();
+
+
+  }
+}
+
+
+A, E, I, O, U, L, N, R, S, T       1
+D, G                               2
+B, C, M, P                         3
+F, H, V, W, Y                      4
+K                                  5
+J, X                               8
+Q, Z                               10
--- Scrable/Models/Program.cs
+++ Scrable/Models/Program.cs
@@ -5,11 +5,18 @@ namespace WordValue
 {
   public class Program
   {
+    public static void Main()
+    {
+      Console.WriteLine("enter a word for scrable");
+      string ScrableWord = Console.ReadLine();
+      Char [] ScrableWordArray = ScrableWord.ToCharArray();
+      Console.WriteLine($"here is our firstArray {ScrableWordArray[0]}");
+      Console.WriteLine($"here is our firstArray {ScrableWord[0]}");
+
+      
 
 
 
-
-
-    
+    }
   }
 }
