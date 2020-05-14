fe975b94867c7a1a347e647440968908463e0485
nkhakwan <post_khan@yahoo.com>
2020-05-14 15:40:24 -0700

    committing a fully functionable scrable word valueing code minus tests

--- Scrable/Models/Backend.cs
+++ Scrable/Models/Backend.cs
@@ -4,8 +4,42 @@ using System.Collections.Generic;
 namespace WordValue{
   public class MyWord
   {
-
-    public static Dictionary<int,string> ValueContainer= new Dictionary <int,string> { {1,"aeioulnrst"}, {2,"dg"}, {3,"bcmp"}, {4, "fhvwy"},{5, "k"},{6,"jx"},{7,"qz"} };
+    public static Dictionary<int,string> ValueContainer {get; set;}
+    public int TotalValue {get; set;}
+    public MyWord()
+    {
+      ValueContainer = new Dictionary <int,string> { {1,"aeioulnrst"}, {2,"dg"}, {3,"bcmp"}, {4, "fhvwy"},{5, "k"},{6,"jx"},{7,"qz"} };
+      TotalValue =0;
+    }
+    
+    public void CalculateScrable (string ScrableWords)
+    {
+      string ScrableWord = ScrableWords.ToLower();
+      char [] ScrableWordArray = ScrableWord.ToCharArray();
+      
+      for (int i=0; i<ScrableWordArray.Length; i++)
+      {
+        for (int j=1; j<MyWord.ValueContainer.Count+1;j++)
+        {
+          if (MyWord.ValueContainer[j].Contains(ScrableWordArray[i]))
+          {
+            if (j<6)
+            {
+              TotalValue +=j ;
+            }
+            else if(j==6)
+            {
+              TotalValue +=8;
+            }
+            else if (j==7)
+            {
+              TotalValue +=10;
+            }
+              
+          }
+        }
+      }
+    }
 
 
   }
--- Scrable/Models/Program.cs
+++ Scrable/Models/Program.cs
@@ -9,32 +9,9 @@ namespace WordValue
     {
       Console.WriteLine("Man enter a word and i'll calculate its scrable value");
       string ScrableWords = Console.ReadLine();
-      string ScrableWord = ScrableWords.ToLower();
-      char [] ScrableWordArray = ScrableWord.ToCharArray();
-      int TotalValue=0;
-      for (int i=0; i<ScrableWordArray.Length; i++)
-      {
-        for (int j=1; j<MyWord.ValueContainer.Count+1;j++)
-        {
-          if (MyWord.ValueContainer[j].Contains(ScrableWordArray[i]))
-          {
-            if (j<6)
-            {
-              TotalValue +=j ;
-            }
-            else if(j==6)
-            {
-              TotalValue +=8;
-            }
-            else if (j==7)
-            {
-              TotalValue +=10;
-            }
-              
-          }
-        }
-      }
-      Console.WriteLine($"Here is the answer{TotalValue}");
+      MyWord MyWordInstance = new MyWord();
+      MyWordInstance.CalculateScrable(ScrableWords);
+      Console.WriteLine($"Here is the answer{MyWordInstance.TotalValue}");
 
           
 
