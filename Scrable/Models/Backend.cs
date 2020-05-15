883103106de314bc91bbe380a80170f4d9444ce6
nkhakwan <post_khan@yahoo.com>
2020-05-14 17:15:33 -0700

    committing some cleaning up of file

--- Scrable/Models/Backend.cs
+++ Scrable/Models/Backend.cs
@@ -16,7 +16,6 @@ namespace WordValue{
     {
       string ScrableWord = ScrableWords.ToLower();
       char [] ScrableWordArray = ScrableWord.ToCharArray();
-      
       for (int i=0; i<ScrableWordArray.Length; i++)
       {
         for (int j=1; j<MyWord.ValueContainer.Count+1;j++)
@@ -35,7 +34,6 @@ namespace WordValue{
             {
               TotalValue +=10;
             }
-              
           }
         }
       }
